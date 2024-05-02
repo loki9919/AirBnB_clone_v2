47592ee87dec28af1c05b979f219d90f556efcbf
lokmane rouijel <lokmane9919@gmail.com>
2024-05-02 02:35:10 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
 from models import storage
+from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
