1c04360be72041f69ee8d1b555bbb0d1d836ad3a
lokmane rouijel <lokmane9919@gmail.com>
2024-05-02 02:44:43 +0100

    l3adab

---
+++ models/engine/db_storage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""New engine DBStorage"""
+import json
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+all_classes = {'State': State, 'City': City, 'User': User,
+               'Place': Place, 'Review': Review, 'Amenity': Amenity}
+
+
+class DBStorage:
+    """Save in the database
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Creates the engine to the database.
+        """
+        var_eng = "{0}+{1}://{2}:{3}@{4}:3306/{5}".format(
+            'mysql', 'mysqldb', getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'), getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB'))
+        self.__engine = create_engine(var_eng, pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """"manage dict all cls and entities"""
+        new_dict = {}
+        if cls is not None:
+            if cls in all_classes:
+                see = self.__session.query(all_classes[cls])
+            else:
+                see = self.__session.query(cls)
+            for instance in see:
+                key = instance.__class__.__name__ + "." + instance.id
+                new_dict[key] = instance
+        if cls is None:
+            for clas in all_classes.keys():
+                see = self.__session.query(all_classes[clas])
+                for instance in see:
+                    key = instance.__class__.__name__ + "." + instance.id
+                    new_dict[key] = instance
+        return (new_dict)
+
+    def new(self, obj):
+        """Add obj to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables into database and initialize a new session"""
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
+
+    def close(self):
+        """call remove() method on the private session attribute
+        """
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+
+    __tablename__ = "states"
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """Return a list of City instance
+            """
+            city_list = []
+            for _id, city in models.storage.all(City).items():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
