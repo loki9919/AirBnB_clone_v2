bd1d373077356c52d35f6800cf758d0160ee1fa2
lokmane rouijel <lokmane9919@gmail.com>
2024-05-02 03:07:34 +0100

    lekher

--- models/review.py
+++ models/review.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import getenv
+""" Review module for the HBNB project """
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """This is the class for review
-    """
-    __tablename__ = "reviews"
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
