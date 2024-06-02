a116686dcdd841f2991b71d14e43683558601020
lokmane rouijel <lokmane9919@gmail.com>
2024-06-02 19:41:18 +0100

    setup web static

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application:
+     web application must be listening on 0.0.0.0, port 5000
+     Routes:
+        /: display "Hello HBNB!"
+     must use the option strict_slashes=False in my route definition
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
